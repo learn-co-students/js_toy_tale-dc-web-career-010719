c0842f27bfb34c8c542cb0181b53e8464b6438bd
github username <github email address>
2019-02-24 21:17:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const toySubmitBtn = document.querySelector('.submit')
 let addToy = false
 
 // YOUR CODE HERE
 
-addBtn.addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', init)
+
+function init() {
+
+  displayToys()
+  addBtn.addEventListener('click', toggleForm)
+  toySubmitBtn.addEventListener('click', createToy)
+}
+
+function displayToys() {
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(json => json.forEach(renderToy))
+}
+
+function toggleForm() {
   // hide & seek with the form
   addToy = !addToy
   if (addToy) {
@@ -13,36 +29,43 @@ addBtn.addEventListener('click', () => {
   } else {
     toyForm.style.display = 'none'
   }
-})
-
-// OR HERE!
-fetch('http://localhost:3000/toys')
-.then(resp => resp.json())
-.then(json => json.forEach(renderToy))
-
-function renderToy(toy) {
-  // let div = document.createElement('div')
-  // div.class = "card"
-  // div.id = `${toy.id}`
-  // let h2 = document.createElement('h2')
-  // h2.innerText = `${toy.name}`
-  // div.appendChild(h2)
-  // div.innerHTML += `<img src=${toy.image} class="toy-avatar" />`
-  //
-  toyHTML =
-    `<div class="card" id="${toy.id}">
-      <h2>${toy.name}</h2>
-      <img src=${toy.image} class="toy-avatar" />
-      <p>${toy.likes} Likes </p>
-      <button class="like-btn">Like <3</button>
-    </div>`
-  document.getElementById('toy-collection').innerHTML += toyHTML
-  // let newLikeBtn = document.getElementById('toy-collection').lastChild.querySelector('button')
-  // newLikeBtn.addEventListener('click', () => {console.log('hi')})
 }
 
-let createButton = document.querySelectorAll('input')[2]
-createButton.addEventListener('click', createToy)
+function renderToy(toy) {
+  let div = document.createElement('div')
+  document.getElementById('toy-collection').appendChild(div)
+  div.classList.add("card")
+  div.dataset.id = `${toy.id}`
+  div.id = `toy-${toy.id}`
+
+  let h2 = document.createElement('h2')
+  h2.innerText = `${toy.name}`
+  div.appendChild(h2)
+
+  let img = document.createElement('img')
+  img.src = toy.image
+  img.classList.add('toy-avatar')
+  div.appendChild(img)
+
+  let p = document.createElement('p')
+  p.innerText = `${toy.likes} Likes`
+  div.appendChild(p)
+
+  let likeBtn = document.createElement('button')
+  likeBtn.classList.add('like-btn')
+  likeBtn.innerText = 'Like <3'
+  div.appendChild(likeBtn)
+  likeBtn.addEventListener('click', likeToy)
+
+  // toyHTML =
+  //   `<div class="card" id="${toy.id}">
+  //     <h2>${toy.name}</h2>
+  //     <img src=${toy.image} class="toy-avatar" />
+  //     <p>${toy.likes} Likes </p>
+  //     <button class="like-btn">Like <3</button>
+  //   </div>`
+  // document.getElementById('toy-collection').innerHTML += toyHTML
+}
 
 function createToy(e) {
   e.preventDefault()
@@ -61,30 +84,32 @@ function createToy(e) {
     body: JSON.stringify(data)
   })
   .then(resp => resp.json())
-  .then(json => renderToy)
+  .then(json => renderToy(data))
 }
 
-// document.querySelectorAll('.like-btn').addEventListener('click', likeToy)
-// document.querySelector('.like-btn').parentElement.id
-
 function likeToy() {
-  let currentId = document.querySelector('.like-btn').parentElement.id
-  fetch(`http://localhost:3000/toys/${currentId}`, {
-    method: 'POST',
+  let card = this.parentElement
+  let currentLikes = parseInt(card.children[2].innerText.split(' ')[0])
+  let newLikes = currentLikes + 1
+  console.log('this part works fine')
+  fetch(`http://localhost:3000/toys/${card.dataset.id}`, {
+    method: 'PATCH',
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json"
     },
-    body: {likes: 10}
+    body: JSON.stringify({likes: newLikes})
   })
   .then(resp => resp.json())
-  .then(json => console.log(json))
+  .then(json => () => {
+    debugger
+    console.log('hi')
+    card.children[2].innerText = `<p>${newLikes} Likes</p>`
+  })
 }
 
-allLikeBtn = document.getElementById('toy-collection').querySelectorAll('button')
-for (let i = 0; i < allLikeBtn.length; i++) {
-  allLikeBtn[i].addEventListener('click', ()=>{console.log('hi')})
-}
+
+
 
 
 
