cf4b5ae692b1df9438c6dd09adfd2921ebdc7243
James Clement <jameseclement@gmail.com>
2019-02-26 10:17:51 -0500

    second solution

--- src/index.js
+++ src/index.js
@@ -17,80 +17,78 @@ addBtn.addEventListener('click', () => {
 
 
 // OR HERE!
-toyCollection = document.querySelector("#toy-collection")
+document.addEventListener("DOMContentLoaded", fetchAllToys)
+let newToyForm = document.querySelector(".add-toy-form")
+newToyForm.addEventListener("submit", createToy)
+let toyCollection = document.querySelector('#toy-collection')
 
-document.addEventListener('DOMContentLoaded', fetchToys)
-newToyForm = document.querySelector(".addToyForm")
-document.addEventListener("submit", createToy)
 
-function fetchToys(){
+function fetchAllToys(){
+  toyCollection.innerHTML = ""
   fetch('http://localhost:3000/toys')
   .then(res => res.json())
   .then(json => json.forEach(toy => renderToy(toy)))
 }
 
 function renderToy(toy){
-  cardDiv = document.createElement('div')
-  cardDiv.classList.add("card")
-  toyCollection.appendChild(cardDiv)
-
-  nameHead = document.createElement("h2")
-  nameHead.innerHTML = toy.name
-  cardDiv.appendChild(nameHead)
-
-  imgTag = document.createElement('img')
-  imgTag.classList.add("toy-avatar")
-  imgTag.src = toy.image
-  cardDiv.appendChild(imgTag)
-
-  likesTag = document.createElement("p")
-  likesTag.innerHTML = `${toy.likes} Likes`
-  cardDiv.appendChild(likesTag)
-
-  button = document.createElement("button")
-  button.dataset.id = `${toy.id}`
-  button.classList.add("like-btn")
-  button.innerHTML = "Like <3"
-  button.addEventListener("click", increaseLikes)
-  cardDiv.appendChild(button)
+  makeToyCard()
+  toyCollection.appendChild(toyCard)
+toyCard.id = toy.id
+toyNameH1.innerHTML = toy.name
+toyImageElement.src = toy.image
+toyLikeButton.dataset.likes = toy.likes
+toyLikesP.innerHTML = `${toyLikeButton.dataset.likes} Likes`
+toyLikeButton.innerText = "Like <3"
+toyLikeButton.addEventListener("click", likeToy)
 }
 
-function increaseLikes(e){
-let currentLikes = e.target.previousElementSibling.innerText
-currentLikesNum = parseInt(currentLikes)
- e.target.previousElementSibling.innerText = `${++currentLikesNum} Likes`
-
-
-fetch(`http://localhost:3000/toys/${e.target.dataset.id}`, {
+function likeToy(e){
+  previousLikes = parseInt(e.target.previousElementSibling.innerHTML)
+  previousLikes++
+  e.target.previousElementSibling.innerHTML = `${previousLikes} Likes`
+  sendData = {"likes": ++e.target.dataset.likes}
+  fetch(`http://localhost:3000/toys/${e.target.parentElement.id}`,{
   method: "PATCH",
   headers:
-  {
+    {
     "Content-Type": "application/json",
-    Accept: "application/json"
-  },
-  body:
-  JSON.stringify({
-    "likes": currentLikesNum
-  })
+    "Accept": "application/json"
+    },
+   body: JSON.stringify(sendData)
+  }
+    )
 
-  })
 }
 
+function makeToyCard(){
+ toyCard = document.createElement('div')
+    toyCard.classList.add("card")
+  toyNameH1 = document.createElement('h1')
+   toyImageElement = document.createElement('img')
+   toyImageElement.classList.add("toy-avatar")
+   toyLikesP = document.createElement('p')
+   toyLikeButton = document.createElement('button')
+    toyLikeButton.classList.add('like-btn')
+
+  toyCard.appendChild(toyNameH1)
+  toyCard.appendChild(toyImageElement)
+  toyCard.appendChild(toyLikesP)
+  toyCard.appendChild(toyLikeButton)
+}
 
 function createToy(e){
-  e.preventDefault
-  data = {
-"name": e.target[0].value ,
-"image": e.target[1].value ,
-"likes": 0
-}
-  fetch('http://localhost:3000/toys', {
+
+let toyInfo = {name: e.target.name.value,
+     image: e.target.image.value,
+      likes: 0}
+
+fetch('http://localhost:3000/toys', {
     method: "POST",
-    headers: {
-  "Content-Type": "application/json",
-  "Accept" : "application/json"
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
     },
-    body: JSON.stringify(data)})
-    .then(res => json(res))
-    .then(json => console.log(json))
+    body: JSON.stringify(toyInfo)
+  }).then(res => fetchAllToys())
 }
