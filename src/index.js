7229e90f88ac8b991f71c803ab4041d06969c223
James Clement <jameseclement@gmail.com>
2019-02-28 13:54:03 -0500

    day 2, much cleaner

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
+const toyFormContainer = document.querySelector('.container')
+const toyForm = document.querySelector('.add-toy-form')
+toyForm.addEventListener("submit", (e)=>addNewToy (e, toyForm))
 let addToy = false
 
 // YOUR CODE HERE
@@ -8,92 +10,78 @@ addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
   if (addToy) {
-    toyForm.style.display = 'block'
+    toyFormContainer.style.display = 'block'
     // submit listener here
   } else {
-    toyForm.style.display = 'none'
+    toyFormContainer.style.display = 'none'
   }
 })
 
 
 // OR HERE!
-document.addEventListener("DOMContentLoaded", fetchAllToys)
-let newToyForm = document.querySelector(".add-toy-form")
-newToyForm.addEventListener("submit", createToy)
-let toyCollection = document.querySelector('#toy-collection')
+document.addEventListener("DOMContentLoaded", () => {
+  fetchToys()
 
+  })
 
-function fetchAllToys(){
-  toyCollection.innerHTML = ""
+function fetchToys() {
   fetch('http://localhost:3000/toys')
   .then(res => res.json())
   .then(json => json.forEach(toy => renderToy(toy)))
-}
-
-function renderToy(toy){
-  let toyCard = document.createElement('div')
-  toyCard.classList.add("card")
-
-  let toyNameH1 = document.createElement('h1')
-  toyNameH1.innerHTML = toy.name
-  toyCard.id = toy.id
-
-  let toyImageElement = document.createElement('img')
-  toyImageElement.src = toy.image
-  toyImageElement.classList.add("toy-avatar")
-
-  let toyLikeButton = document.createElement('button')
-  toyLikeButton.classList.add('like-btn')
-
-  toyLikeButton.innerText = "Like <3"
-
-  toyLikeButton.addEventListener("click", (e) => likeToy(e, toy))
-
-  let toyLikesP = document.createElement('p')
-  toyLikesP.innerHTML = `${toy.likes} Likes`
-
-
-  toyCard.appendChild(toyNameH1)
-  toyCard.appendChild(toyImageElement)
-  toyCard.appendChild(toyLikesP)
-  toyCard.appendChild(toyLikeButton)
-  toyCollection.appendChild(toyCard)
-}
-
-function likeToy(e, toy){
-  let toyLikes = ++toy.likes
-  displayedLikes = e.target.parentElement.children[2].innerText.split(" ")[0]
-  ++displayedLikes
-  e.target.parentElement.children[2].innerText = `${displayedLikes} Likes`
-  sendData = {"likes": toyLikes}
-  fetch(`http://localhost:3000/toys/${toy.id}`,{
-  method: "PATCH",
-  headers:
-    {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-    },
-   body: JSON.stringify(sendData)
   }
-    )
 
+  function renderToy(toy) {
+    let toyCollection = document.querySelector("#toy-collection")
+    let toyCard = document.createElement("div")
+    toyCard.classList.add("card")
+    let name = document.createElement("h2")
+    name.innerHTML = toy.name
+    let image = document.createElement("img")
+    image.src = toy.image
+    image.classList.add("toy-avatar")
+    let likesP = document.createElement("p")
+    likesP.innerHTML = `${toy.likes} Likes`
+    let likeButton = document.createElement("button")
+    likeButton.classList.add("like-btn")
+    likeButton.innerHTML = "Like <3"
+    likeButton.addEventListener("click", (e)=> likeToy(e, toy))
+    toyCard.append(name, image, likesP, likeButton)
+    toyCollection.appendChild(toyCard)
+  }
+
+function addNewToy(e, toyForm){
+  e.preventDefault
+  let toy = {
+    "name": `${toyForm.name.value}`,
+    "image": `${toyForm.image.value}`,
+    "likes": 0
+  }
+  renderToy(toy)
+  fetch('http://localhost:3000/toys', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(toy)
+  })
 }
 
+function likeToy(e, toy) {
 
-function createToy(e){
+  shownLikes = e.target.previousElementSibling.innerHTML.split(' ')[0]
+  e.target.previousElementSibling.innerHTML = `${++shownLikes} Likes`
+let data = {
+  "likes": shownLikes
+    }
 
-let toyInfo = {name: e.target.name.value,
-     image: e.target.image.value,
-      likes: 0}
-
-fetch('http://localhost:3000/toys', {
-    method: "POST",
+  fetch (`http://localhost:3000/toys/${toy.id}`, {
+    method: "PATCH",
     headers:
     {
       "Content-Type": "application/json",
       Accept: "application/json"
     },
-    body: JSON.stringify(toyInfo)
-  }).then(res => res.json())
-  .then(json => renderToy(json))
+    body: JSON.stringify(data)
+  })
 }
